a0380578d4eaa3cc903d4cf133ded9e443384265
Youssef68Oubenhamou <youssefouben8@gmail.com>
2025-05-13 16:51:30 +0100

    controller: building the update user function !

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,32 @@
-export const userController = async (req , res) => {
+import User from "../models/userModel.js";
+
+export const createUser = async (req , res) => {
 
     try {
         
         const user = new User(req.body);
 
         res.status(201).json(user);
+
+    } catch (error) {
+        
+        console.log(error);
+
+    }
+
+}
+
+export const getUser = async (req , res) => {
+
+    const { id: _id } = req.params;
+
+    const newPost = req.body;
+
+    try {
+        
+        const post = await User.updateOne(_id , newPost);
+
+        res.status(201).json(post);
 
     } catch (error) {
         
