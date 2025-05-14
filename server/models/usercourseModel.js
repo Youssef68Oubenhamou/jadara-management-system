6c9a83cef72bff7440cf16a343a664432163e06e
Tommy <lbaizoussam537@gmail.com>
2025-05-14 15:27:27 +0100

    setting up the Schema

---
+++ server/models/usercourseModel.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const usercourseSchema = new Schema({
+
+    user_id : {
+        type : String,
+        required: true
+    },
+
+    course_id: {
+        type: String,
+        required: true
+    },
+   
+});
+
+export const Usercourse = mongoose.model('Usercourse', usercourseSchema);
