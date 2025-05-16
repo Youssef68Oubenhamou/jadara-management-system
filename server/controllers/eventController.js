9fa97d07b337c23a4372f401e46004dd728fb839
Tommy <lbaizoussam537@gmail.com>
2025-05-16 19:26:31 +0100

    add:add event controller

---
+++ server/controllers/eventController.js
@@ -0,0 +1,43 @@
+import Event from "../models/eventeModel.js"
+
+
+
+// -------Get all events------  
+export const getEvents = async (req, res) => {
+  try {
+    const Events = await Event.find();
+    res.status(200).json(Events);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching event'});
+  }
+};
+// -------Create event---------
+export const createEvent = async (req, res) => {
+  try {
+    const newEvent = new Event(req.body);
+    await newEvent.save();
+    res.status(201).json(newEvent);
+  } catch (error) {
+    res.status(400).json({ message: 'Error creating event' });
+  }
+};
+// -------Update event---------
+export const updateEvent = async (req, res) => {
+  try {
+    const event = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!event) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+// --------Delete Event----------
+export const deleteEvent = async (req, res) => {
+  try {
+    const event = await Event.findByIdAndDelete(req.params.id);
+    if (!event) return res.status(404).json({ message: 'User not found' });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
