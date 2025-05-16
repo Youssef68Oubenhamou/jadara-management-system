667035e6113fdf12b2e3cfa943d8564f0a4a4316
Youssef68Oubenhamou <youssefouben8@gmail.com>
2025-05-16 10:21:22 +0100

    fix: corrected an issue in verifyToken middleware !

--- server/middlewares/authJwt.js
+++ server/middlewares/authJwt.js
@@ -6,7 +6,7 @@ export default (req , res , next) => {
 
     try {
 
-        const token = req.headers.authorization.replace("Bearer " , "");
+        const token = req.headers.authorization.split(" ")[1];
         console.log(token);
         const decoded = jwt.verify(token , process.env.JWT_SECRET);
         console.log(decoded);
