0d2acb40f875bf7ee209c0049d4fda70c79739c2
Soufiane18X <slhbal18@gmail.com>
2025-05-18 15:47:43 +0100

    adding middleware routes

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,10 +1,11 @@
 import express from "express"
 import {getEvents,createEvent,updateEvent,deleteEvent} from "../controllers/eventController.js"
+import verifyRole from "../middlewares/verifyRole.js"
 
 const router = express.Router();
-router.post('/create',createEvent)
-router.get('/get',getEvents)
-router.put('/update/:id',updateEvent)
-router.delete('/remove/:id',deleteEvent )
+router.post('/create' , verifyRole , createEvent)
+router.get('/get' , getEvents)
+router.put('/update/:id' , verifyRole , updateEvent)
+router.delete('/remove/:id' , verifyRole , deleteEvent )
 
 export default router;
