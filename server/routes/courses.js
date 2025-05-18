dbc2647196a8ea389a84008563f25fcea375d49f
boutadoute <mohamedboutadoute33@gmail.com>
2025-05-18 15:48:28 +0100

    fix:adding middlewares routes

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -1,13 +1,15 @@
   import express from "express";
+  import verifyRole from "../middlewares/verifyRole.js"
   import {createCourse , getCourses , updateCourse, deleteCourse} from "../controllers/courseController.js"
   
   const router = express.Router();
   
   
-  router.post('/create', createCourse);
+  router.post('/create', verifyRole ,createCourse);
   router.get('/get', getCourses);
-  router.put('/update/:id', updateCourse);
-  router.delete('/remove/:id', deleteCourse);
+  router.put('/update/:id', verifyRole,updateCourse);
+  router.delete('/remove/:id', verifyRole ,deleteCourse);
+
 
   export default router;
 
