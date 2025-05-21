21d1fb69e2cdf530ba50bfc8baf6e0701e6d98a5
Youssef68Oubenhamou <youssefouben8@gmail.com>
2025-05-22 00:03:24 +0100

    feat: adding layout !

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,32 @@
-import { Button } from "@/components/ui/button"
+import './App.css';
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import Layout from './components/layout';
+import { ThemeProvider } from "./context/context-provider";
+import { QueryClient , QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
 
     return (
-        <>
-            <div className="flex flex-col items-center justify-center min-h-svh">
-                <Button>Click me</Button>
-            </div>
-        </>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ThemeProvider defaultTheme="dark">
+
+                    <Layout>
+
+                        <Routes>
+                            <Route path="/" element={"dashboard"} />
+                            <Route path="/city/:cityName" element={"student"} />
+                        </Routes>
+
+                    </Layout>
+
+                </ThemeProvider>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     )
 }
 
