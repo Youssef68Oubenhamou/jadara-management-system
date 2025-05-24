db9d74f7b98f6f7f399aa0e767a72aa50d950548
Tommy <lbaizoussam537@gmail.com>
2025-05-25 00:21:54 +0100

    feat:add login and register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
-import { Button } from "@/components/ui/button"
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import logo from './jadaralogo-8ba458e5.png';
 
-function App() {
+const App = () => {
+  return (
+    <Router>
+      <div className="bg flex items-center justify-center min-h-screen">
+        <div className="w-full max-w-sm text-center">
+          <img src={logo} alt="Logo" className="mb-4 w-24 mx-33" />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
 
-    return (
-        <>
-            <div className="flex flex-col items-center justify-center min-h-svh">
-                <Button>Click me</Button>
-            </div>
-        </>
-    )
-}
-
-export default App
+export default App;
