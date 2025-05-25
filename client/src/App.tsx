150ce27eb2fe7729772f08990c12595d9ec07217
Soufiane18X <slhbal18@gmail.com>
2025-05-25 15:34:06 +0100

    feat: adding student list feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Button } from "@/components/ui/button"
+import StudentList from "./components/student";
 
+// import React from "react";
 function App() {
 
     return (
         <>
             <div className="flex flex-col items-center justify-center min-h-svh">
-                <Button>Click me</Button>
             </div>
+            <StudentList/>
         </>
     )
 }
